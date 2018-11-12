18a0843b9ba4a969a3307e72b735efaaf4bc658a
Daniel Dopierała <dopierala.daniel@gmail.com>
2018-11-12 13:41:23 +0100

    Basic Spring application setup

---
+++ src/main/java/pl/dopierala/ia/Application.java
@@ -0,0 +1,18 @@
+package pl.dopierala.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application's main class.
+ *
+ * @author Daniel Dopierała
+ * @since 2018/11/12
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
